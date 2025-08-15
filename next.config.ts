fc310415f772da1a021b9ead510bb053959880e7
Ali <ali86144@gmail.com>
2025-08-15 23:56:32 +0930

    pages updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '',        // user site => no subpath
+  assetPrefix: '',     // user site => no subpath
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
